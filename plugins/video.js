e6bf69e32f308cf50a2df37c2485d16d84b71fe7
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 00:23:56 -0600

    ©ShadowBot

--- plugins/video.js
+++ plugins/video.js
@@ -1,19 +1,52 @@
-let util = require('util')
-let path = require('path')
-let fs = require('fs')
-
-let caption = `
-🥵🔥
-`.trim()
-
-let handler = async (m, { conn }) => {
-let video = fs.readFileSync(`./videos/${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41'])}.mp4`)
-
-     conn.sendFile(m.chat, video, '1.mp4', caption, m)
+let handler = async(m, { conn, usedPrefix, command }) => {
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🥵🔥', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
-handler.command = /^video|vídeo$/i
+handler.command = /^video$/i
 module.exports = handler
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
+  return list[Math.floor(list.length * Math.random())]
 }
+
+const asupan = [
+"https://l.top4top.io/m_2235dduf01.mp4",
+"https://a.top4top.io/m_2235268m61.mp4",
+"https://b.top4top.io/m_2235k7hze2.mp4",
+"https://c.top4top.io/m_2235lxohb3.mp4",
+"https://d.top4top.io/m_2235jwd2e4.mp4",
+"https://e.top4top.io/m_2235h5b1z5.mp4",
+"https://f.top4top.io/m_2235gihcu6.mp4",
+"https://l.top4top.io/m_2235dp7m41.mp4",
+"https://a.top4top.io/m_2235zxue82.mp4",
+"https://b.top4top.io/m_2235m3bhf3.mp4",
+"https://c.top4top.io/m_2235vjyio4.mp4",
+"https://d.top4top.io/m_2235m9tdu5.mp4",
+"https://e.top4top.io/m_2235y2kon6.mp4",
+"https://f.top4top.io/m_2235rhid57.mp4",
+"https://g.top4top.io/m_2235zgsqf8.mp4",
+"https://i.top4top.io/m_2235drxxg10.mp4",
+"https://d.top4top.io/m_2235fzynm1.mp4",    
+"https://e.top4top.io/m_22354t3zk2.mp4",
+"https://f.top4top.io/m_2235gyxgh3.mp4",
+"https://g.top4top.io/m_22357cmft4.mp4", 
+"https://i.top4top.io/m_2235mcizm6.mp4",
+"https://j.top4top.io/m_2235gwsn17.mp4",
+"https://k.top4top.io/m_2235gzzjc8.mp4",
+"https://a.top4top.io/m_2235l9y1310.mp4",  
+"https://l.top4top.io/m_2235r1opz1.mp4",    
+"https://a.top4top.io/m_22358cuuu2.mp4",
+"https://b.top4top.io/m_22350c9br3.mp4",
+"https://c.top4top.io/m_22355p2js4.mp4",  
+"https://d.top4top.io/m_2235lv7415.mp4",
+"https://e.top4top.io/m_2235q8z3f6.mp4",     
+"https://b.top4top.io/m_22358oas31.mp4",
+"https://c.top4top.io/m_2235xg7o62.mp4",
+"https://d.top4top.io/m_2235ut91p3.mp4",
+"https://e.top4top.io/m_22352ktoj4.mp4",
+"https://f.top4top.io/m_2235hcqj65.mp4",
+"https://g.top4top.io/m_2235j81s76.mp4", 
+"https://d.top4top.io/m_2235993nu1.mp4",
+"https://c.top4top.io/m_22352lrsd1.mp4",
+"https://g.top4top.io/m_2235pi8791.mp4",
+"https://f.top4top.io/m_2235sxi5y1.mp4"
+]
