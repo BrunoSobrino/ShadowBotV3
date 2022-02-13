1b31ff3a3f300b05966bf48eb47b7b378f416a50
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 17:59:48 -0600

    ©ShadowBot

--- plugins/grup-seting.js
+++ plugins/grup-seting.js
@@ -3,6 +3,8 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 	let isClose = { // Switch Case Like :v
 		'abierto': false,
 		'cerrado': true,
+                'abrir': false,
+		'cerrar': true,
 	}[(args[0] || '')]
 	await conn.updatePresence(m.chat, Presence.composing)
 	if (isClose === undefined)
@@ -10,8 +12,8 @@ let handler  = async (m, { conn, args, usedPrefix, command }) => {
 *[ ⚠ ️] Formato erróneo!!*
 
 *┏━━━❲ ✨Ejemplo✨ ❳━━━┓* 
-*┠┉↯ ${usedPrefix + command} cerrado*
-*┠┉↯ ${usedPrefix + command} abierto*
+*┠┉↯ ${usedPrefix + command} abrir*
+*┠┉↯ ${usedPrefix + command} cerrar*
 `.trim()
 	await conn.groupSettingChange(m.chat, GroupSettingChange.messageSend, isClose)
 {m.reply('*Se realizó con exito✅*')}
