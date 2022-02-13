48a1b57fabe841562da0bfd8f6987246cbcf9b72
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-12 22:20:44 -0600

    ©ShadowBot

--- plugins/logosad.js
+++ plugins/logosad.js
@@ -5,5 +5,5 @@ if (!args[0]) throw '*Ingrese un texto*'
 let res = `https://bx-hunter.herokuapp.com/api/canvas/gfx3?apikey=FuckBitch&text1=${response[0]}&text2=${response[1]}`
 conn.sendFile(m.chat, res, 'error.jpg', `SAD BOY`, m, false)
 }
-handler.command = /^(lsad)$/i
+handler.command = /^(logosad)$/i
 module.exports = handler
