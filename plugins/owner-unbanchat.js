935143717abacd38324a684dbc9b37b01ddc07e4
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 14:03:57 -0600

    ©ShadowBot

---
+++ plugins/owner-unbanchat.js
@@ -0,0 +1,10 @@
+let handler = async (m, { conn }) => {
+  if (!(m.chat in global.DATABASE._data.chats)) return m.reply('*Este chat no está registrado en la base de datos!*')
+  let chat = global.DATABASE._data.chats[m.chat]
+  if (!chat.isBanned) return m.reply('*Este chat no está prohibido!!*')
+  chat.isBanned = false
+  m.reply('*✅ Listo.. chat desbaneado!*')
+}
+handler.command = /^unbanchat2$/i
+handler.rowner = true
+module.exports = handler
