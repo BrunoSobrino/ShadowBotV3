7f14e8b3e5d896cc0cbc94a53cb488813d9fbf82
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 13:47:27 -0600

    ©ShadowBot

--- plugins/translate.js
+++ plugins/translate.js
@@ -1,24 +1,37 @@
-let fetch = require('node-fetch')
+const translate = require('translate-google-api')
+const defaultLang = 'es'
+const tld = 'cn'
 let handler = async (m, { args, usedPrefix, command }) => {
-    let msg = `*[❗] Uso correcto del comando ${usedPrefix}${command} idioma texto*\n*Ejemplo:*\n*${usedPrefix}${command} es Hello*`
-    if (!args || !args[0]) return m.reply(msg)
-    let lang = 'es'
-    let text = args.join(' ')
-    if (args[0].length == 2 && args.length > 0) {
-        lang = args[0]
-        text = args.slice(1).join(' ')
-    }
-    let res = await fetch(global.API('bg', '/translate', {
-        q: text,
-        lang
-    }))
-    let json = await res.json()
-    if (json.status !== true) throw json
-    m.reply(`
-*${json.result.from.language.iso}:* ${text}
-*${lang}:* ${json.result.text}
-`.trim())
+let msg = `*[❗] Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos aquí:*\n*- https://cloud.google.com/translate/docs/languages*`
+if (!args || !args[0]) return m.reply(msg)
+let err = `
+Contoh:
+${usedPrefix + command} <lang> [text]
+${usedPrefix + command} id your messages
+Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
+`.trim()
+let lang = args[0]
+let text = args.slice(1).join(' ')
+if ((args[0] || '').length !== 2) {
+lang = defaultLang
+text = args.join(' ')
 }
+if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+let result
+try {
+result = await translate(`${text}`, {
+tld,
+to: lang,
+})
+} catch (e) {
+result = await translate(`${text}`, {
+tld,
+to: defaultLang,
+})
+throw err
+} finally {
+m.reply(result[0])
+}}
 handler.command = /^(tr(anslate)|traducir?)$/i
-
+handler.fail = null
 module.exports = handler
