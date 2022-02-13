c66a08782cc3fda68af9938345853d72d1064b4e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 15:55:04 -0600

    ©ShadowBot

---
+++ plugins/sc.js
@@ -0,0 +1,16 @@
+//Codigo by ShadowBotV3
+let fetch = require('node-fetch')
+let handler = async (m, { conn, usedPrefix }) => {
+let who = m.sender
+let pp = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
+let kai = await(await fetch('https://i.imgur.com/oHrJTtq.jpg')).buffer()
+let text = `*https://github.com/BrunoSobrino/ShadowBotV3*`.trim()
+conn.reply(m.chat, text, m, { thumbnail: kai, contextInfo: {
+mentionedJid: [who],
+externalAdReply: {
+title: '𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃',
+body: '🤖 Ｓｈａｄｏｗ Ｂｏｔ 🤖',
+thumbnail: pp
+}}})}
+handler.command = /^sc|script|repositorio|repobot|scbot$/i
+module.exports = handler
