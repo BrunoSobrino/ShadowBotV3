e76560a899adaf2c9cc322b2285b7bcc2a86274e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-13 13:55:52 -0600

    ©ShadowBot

--- plugins/translate.js
+++ plugins/translate.js
@@ -4,34 +4,23 @@ const tld = 'cn'
 let handler = async (m, { args, usedPrefix, command }) => {
 let msg = `*[❗] Uso correcto del comando ${usedPrefix + command} (idioma) (texto)*\n*Ejemplo:*\n*${usedPrefix + command} es Hello*\n\n*Conoce los idiomas admitidos aquí:*\n*- https://cloud.google.com/translate/docs/languages*`
 if (!args || !args[0]) return m.reply(msg)
-let err = `
-Contoh:
-${usedPrefix + command} <lang> [text]
-${usedPrefix + command} id your messages
-Daftar bahasa yang didukung: https://cloud.google.com/translate/docs/languages
-`.trim()
 let lang = args[0]
 let text = args.slice(1).join(' ')
 if ((args[0] || '').length !== 2) {
 lang = defaultLang
-text = args.join(' ')
-}
+text = args.join(' ') }
 if (!text && m.quoted && m.quoted.text) text = m.quoted.text
 let result
 try {
 result = await translate(`${text}`, {
 tld,
-to: lang,
-})
+to: lang, })
 } catch (e) {
 result = await translate(`${text}`, {
 tld,
-to: defaultLang,
-})
-throw err
+to: defaultLang, })
 } finally {
-m.reply(result[0])
-}}
+m.reply('*Traducción:* ' +  result[0]) }}
 handler.command = /^(tr(anslate)|traducir?)$/i
 handler.fail = null
 module.exports = handler
