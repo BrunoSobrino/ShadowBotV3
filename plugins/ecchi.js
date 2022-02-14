614d48622f2aed37d0fb0d37fabe03822d5d2da5
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-14 04:54:00 -0600

    ©ShadowBot

--- plugins/ecchi.js
+++ plugins/ecchi.js
@@ -2,10 +2,10 @@ const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
 let res = await axios("https://meme-api.herokuapp.com/gimme/ecchi")
 let json = res.data
-let url = json.url
+let ShadowBot = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.sendButtonImg(m.chat, url, "*Sexy ❤️‍🔥*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+conn.sendButtonImg(m.chat, ShadowBot, "*Sexy ❤️‍🔥*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^ecchi$/i
 module.exports = handler
 module.exports = handler
