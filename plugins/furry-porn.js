d7c9b58d0250b532ae0a8a0ee382e135f6ac08ae
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-14 00:33:52 -0600

    ©ShadowBot

--- plugins/furry-porn.js
+++ plugins/furry-porn.js
@@ -1,21 +1,8 @@
 const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/Furryporn')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `*¿Si me pongo de perrito, me adoptas? 🔥🥴*`, m)
-  }
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://meme-api.herokuapp.com/gimme/Furryporn")
+let json = res.data
+let url = json.url
+conn.send2ButtonImg(m.chat, url, "*¿Si me pongo de perrito, me adoptas? 🔥🥴*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`)}
 handler.command = /^(furry|furro|furr@|furra|furrys|furros|furr@s|furras|furri|furris)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
 module.exports = handler
