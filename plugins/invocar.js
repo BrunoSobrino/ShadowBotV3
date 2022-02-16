2ff2e37e47ab70311d72569ecfe6e9d88335587b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-15 18:40:11 -0600

    ©ShadowBot

--- plugins/invocar.js
+++ plugins/invocar.js
@@ -2,19 +2,25 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, participants, args }) => {
 const getGroupAdmins = (participants) => { admins = []
 for (let i of participants) { i.isAdmin ? admins.push(i.jid) : '' } return admins }
-const mentions = (teks, memberr, id) => { (id == null || id == undefined || id == false) ? conn.sendMessage(m.chat, teks.trim(), MessageType.extendedText, { contextInfo: { "mentionedJid": memberr } }) : conn.sendMessage(m.chat, teks.trim(), MessageType.extendedText, { quoted: m, contextInfo: { "mentionedJid": memberr } })}
+const mentions = (teks, memberr, id) => { (id == null || id == undefined || id == false) ? conn.sendMessage(m.chat, teks.trim(), MessageType.extendedText, { contextInfo: { "mentionedJid": memberr } }) : 
+conn.sendButton(m.chat, teks.trim(), '©The Shadow Brokers - Bot', 'VOLVER A INVOCAR', `#invocar`, MessageType.extendedText, { quoted: m, contextInfo: { "mentionedJid": memberr } })}
 const isGroup = m.chat.endsWith('@g.us')
 let grupmeta = await conn.groupMetadata(m.chat)
 const groupMembers = isGroup ? grupmeta.participants : ''
 const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
 const info = await conn.groupMetadata(m.chat)
+let vn = './media/Invocar.mp3'
 let users = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
 let pesan = args.join` `
 let oi = `*MENSAJE:* ${pesan}`
 let hmm = `*⺀I N V O C A N D O - G R U P O⺀*\n\n`
 let duh = `└\n\n*▌│█║▌║▌║║▌║▌║▌║█*` 
 var teks = `${oi}\n\n❏\n`
-for (let admon of groupMembers) { teks += `┣➥ @${admon.jid.split('@')[0]}\n`} mentions(hmm+teks+duh, users, true,{ contextInfo: { mentionedJid: users } })}
+for (let admon of groupMembers) { teks += `┣➥ @${admon.jid.split('@')[0]}\n`} mentions(hmm+teks+duh, users, true,{ contextInfo: { mentionedJid: users } })
+await await await await await await conn.sendFile(m.chat, vn, 'Invocar.mp3', null, m, true, {   
+type: 'audioMessage', 
+ptt: true 
+})}
 handler.command = /^invocar|tagall|invocacion|invocación$/i
 handler.group = true
 handler.admin = true
