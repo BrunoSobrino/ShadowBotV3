a20469daf109bd5521a9c4330e5e94f1e56d0739
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-19 13:09:28 -0600

    ©ShadowBot

--- plugins/kpop itzy.js
+++ plugins/kpop itzy.js
@@ -1,23 +1,10 @@
 const axios = require('axios')
-let handler = async(m, { conn }) => {
-let porn = await axios.get('https://meme-api.herokuapp.com/gimme/Itzy')
-           conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
-  }
-handler.help = ['kpopitzy']
-handler.tags = ['images']
-handler.command = /^(kpopitzy)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://meme-api.herokuapp.com/gimme/Itzy")
+let json = res.data
+let ShadowBot = json.url
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+conn.sendButtonImg(m.chat, ShadowBot, "*✨Itzy✨*", '©The Shadow Borkers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
+handler.command = /^(kpopitzy|itzy)$/i
 module.exports = handler
