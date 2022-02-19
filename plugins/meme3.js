e126271be6079e1193d728cede512c7e957ff13a
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-19 14:26:55 -0600

    ©ShadowBot

--- plugins/meme3.js
+++ plugins/meme3.js
@@ -1,22 +1,10 @@
 const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/mememexico')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
-  }
-handler.help = ['meme3']
-handler.tags = ['images']
+let handler = async(m, { conn, usedPrefix, command }) => {
+let res = await axios("https://meme-api.herokuapp.com/gimme/mememexico")
+let json = res.data
+let ShadowBot = json.url
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+conn.sendButtonImg(m.chat, ShadowBot, "*😂😂😂😂😂*", '©The Shadow Borkers - Bot', '😂 SIGUIENTE 😂', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(meme3)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
 module.exports = handler
