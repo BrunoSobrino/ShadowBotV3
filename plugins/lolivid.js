0cdcfade3995c7bf9996095be945ff60f61fc38b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-19 13:25:51 -0600

    ©ShadowBot

--- plugins/lolivid.js
+++ plugins/lolivid.js
@@ -1,10 +1,9 @@
 let fetch = require('node-fetch')
 async function getRandom(url) {
-return Math.floor(Math.random() * url)
-}
-let handler  = async (m, { conn }) => {
-conn.sendFile(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, '', ``, m)
-}
+return Math.floor(Math.random() * url)}
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+conn.sendButtonVid(m.chat, `https://recoders-area.caliph.repl.co/api/lolivid`, "*Loli 😎*", '©The Shadow Borkers - Bot', '😵‍💫 SIGUIENTE 😵‍💫', `${usedPrefix + command}`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(lolivid|lolivideo)$/i
-
 module.exports = handler
