36091250d44b8f560025cc9e7c663d4cc9020bb3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-20 09:53:35 -0600

    ©ShadowBot

--- plugins/estado.js
+++ plugins/estado.js
@@ -1,15 +1,12 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text, usedPrefix }) => {
-  await conn.send3Button(m.chat, `
+let handler = async (m, { conn, command, usedPrefix }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let mentionedJid = [who]
+conn.send3Button(m.chat, `
 *ミ💖 Hola estimado usuario 💖彡*
 
 *ミ🤖 Estado del Bot 🤖彡*
 *=> Bot activo ✔️*
 *=> Bot uso público ✔️*
-`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`)
-}
+`.trim(), '©The Shadow Borkers - Bot', '🔷️ MENÚ 🔷', `${usedPrefix}menu`, '🔶️ MENÚ SIMPLE 🔶️', `${usedPrefix}menusimple`, '💠️ MENÚ AUDIOS 💠️', `${usedPrefix}menuaudios`, m, false, { contextInfo: { mentionedJid }})}
 handler.command = /^(estado|status|estate|state|stado|stats)$/i
-
-handler.exp = 0
-
 module.exports = handler
