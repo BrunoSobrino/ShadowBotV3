b7323ede87baf5a567c0c30f95d93a3f4948b900
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-26 00:18:26 -0600

    ©ShadowBot

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -2,6 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command, args }) => {
 let msg = `*[❗] Uso correcto del comando ${usedPrefix + command} (Autor) (Cancion)*\n*Ejemplo:*\n*${usedPrefix + command} Beret Ojala*`
 if (!args || !args[0]) return m.reply(msg)
+try {
 let res = await fetch(global.API('https://some-random-api.ml', '/lyrics', {
 title: text}))
 if (!res.ok) throw await res.text()
@@ -9,6 +10,9 @@ let json = await res.json()
 if (!json.thumbnail.genius) throw json
 let teks = `*${json.title}*\n_${json.author}_\n\n${json.lyrics}\n\n\n${json.links.genius}`
 let foot = '©The Shadow Brokers - Bot'
-conn.send2ButtonImg(m.chat, await(await fetch(json.thumbnail.genius)).buffer(), teks, foot,'🎧 DESCARGAR MUSICA 🔊', `#play ${text}`, '🎥 DESCARGAR VIDEO 🎞️', `#play2 ${text}`, m)}
-handler.command = /^(l(irik|yrics)|letra)$/i
+conn.send2ButtonImg(m.chat, await(await fetch(json.thumbnail.genius)).buffer(), teks, foot,'🎧 DESCARGAR MUSICA 🔊', `#play ${text}`, '🎥 DESCARGAR VIDEO 🎞️', `#play2 ${text}`, m)
+} catch (e) {
+console.log(e)
+m.reply('*[❗] No se encontro la letra de la canción, por favor prueba con otra*')}}
+handler.command = /^(lol2)$/i
 module.exports = handler
