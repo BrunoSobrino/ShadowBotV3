6ba7db05b93b8fca11db852df120e9df221b711c
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-02-28 22:09:36 -0600

    ©ShadowBot

--- plugins/grupos.js
+++ plugins/grupos.js
@@ -3,15 +3,19 @@ let info = `
 *Hola 👋🏻, unete a los grupos oficiales para pasar un rato agradable usando el Bot o platicando con la familia de The Shadow Borkers - Bot*
 
 *_➤ Grupos oficiales del Bot:_*
-*_1.-_* *https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ*
+*1.- https://chat.whatsapp.com/JlomZPEgo3bLmzjGUYPfyJ*
 
-*_2.-_* *https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs*
+*2.- https://chat.whatsapp.com/DbXBmsydWBE1ZN3EoY0hRs*
 
-*_3.-_* *https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F*
+*3.- https://chat.whatsapp.com/BW0P22xx7EGBTdH5IM851F*
 
-*_4.-_* *https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC*
+*4.- https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKyFC*
 
-*_5.-_* *https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb*
+*5.- https://chat.whatsapp.com/H7NUXdrGlFg20ae3bqgwlb*
+
+*6.- https://chat.whatsapp.com/GvrLSUSzVuT9P17CKfdxDa*
+
+*7.- https://chat.whatsapp.com/HylyPaLc49b9DWF67K8pdc*
 `.trim() 
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*🔥 THE SHADOW BROKERS - BOT 🔥*', 'status@broadcast')
