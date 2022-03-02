8b75f39114d2238431a4185567e81fcdd47341e3
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-01 21:04:26 -0600

    ©ShadowBot

---
+++ plugins/logo sky.js
@@ -0,0 +1,8 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+response = args.join(' ').split('|')
+if (!args[0]) throw '*Ingrese un texto*'        
+let res = `https://api.reysekha.xyz/api/textpro/sky-text?text=${response[0]}&apikey=apirey`
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
+handler.command = /^(logosky)$/i
+module.exports = handler
