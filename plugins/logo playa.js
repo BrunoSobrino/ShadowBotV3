86a3784a6f355e160bf3a13f83e76d7742712c4b
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-05 01:39:07 -0600

    ©ShadowBot

---
+++ plugins/logo playa.js
@@ -0,0 +1,8 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+response = args.join(' ').split('|')
+if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#playa Shadow*'        
+let res = `https://api-alc.herokuapp.com/api/textpro/on-the-beach?texto=${response[0]}&apikey=ConfuMods`
+conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
+handler.command = /^(logoplaya|playa)$/i
+module.exports = handler
