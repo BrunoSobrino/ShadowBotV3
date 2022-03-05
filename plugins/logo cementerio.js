ead6580022cc3083cbf3701a72058f60e3e84587
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-05 00:37:09 -0600

    ©ShadowBot

--- plugins/logo cementerio.js
+++ plugins/logo cementerio.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 response = args.join(' ').split('|')
-if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#logorandom Shadow*'        
+if (!args[0]) throw '*[❗] Ingrese un texto*\n*Ejemplo:*\n*#cementerio Shadow*'        
 let res = `https://api-alc.herokuapp.com/api/photooxy/text-on-scary?texto=${response[0]}&apikey=ConfuMods`
 conn.sendFile(m.chat, res, 'error.jpg', `*Logo terminado*`, m, false)}
 handler.command = /^(cementerio|logocementerio)$/i
