6f7be8f9d9b99155e37cb223df71cc5ef918bc39
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-05 01:48:39 -0600

    ©ShadowBot

--- plugins/yuri.js
+++ plugins/yuri.js
@@ -1,23 +1,7 @@
-const axios = require('axios')
- let handler = async(m, { conn }) => {
-let les = await axios.get('https://meme-api.herokuapp.com/gimme/yuri')
-            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
-  }
-handler.help = ['yuri']
-handler.tags = ['General']
+let fetch = require('node-fetch')
+let handler  = async (m, { conn, usedPrefix, command }) => {
+heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/yuri2?apikey=ConfuMods`)
+json = await heum.buffer()
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
 handler.command = /^(yuri)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
-handler.limit = false
-
 module.exports = handler
