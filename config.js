d37796904fbd94145bb7e26b4a03c9f4b1be0117
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-06 00:27:31 -0600

    ©ShadowBot

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ global.APIKeys = { // APIKey Here
   'https://pencarikode.xyz': 'pais', 
   'https://leyscoders-api.herokuapp.com': 'MIMINGANZ', 
   'https://server-api-rey.herokuapp.com': 'apirey',
-  'https://api.lolhuman.xyz': '85faf717d0545d14074659ad'
+  'https://api.lolhuman.xyz': '9b817532fadff8fc7cb86862'
 }
 
 // Sticker WM
