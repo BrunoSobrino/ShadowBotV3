c66cd586c4a26220018ef05eef7f40f288ab9baa
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-12 14:23:54 -0600

    ©ShadowBot

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,21 +1,17 @@
-let fs = require('fs')
 let handler = m => m
 
 let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
-handler.before = async function (m, { user, isBotAdmin, isAdmin, conn }) {
+handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
   let chat = global.DATABASE.data.chats[m.chat]
   let isGroupLink = linkRegex.exec(m.text)
-  let sticker1 = fs.readFileSync('./src/adios.webp')
-  let sticker2 = fs.readFileSync('./src/adios.webp')
-  let sticker3 = fs.readFileSync('./src/adios.webp')
 
   if (chat.antiLink && isGroupLink) {
     await m.reply(`*「 ANTI LINKS 」*\n*Hasta la vista baby👋, ${await this.getName(m.sender)} rompiste las reglas serás exterminado....!!*`)
     await m.reply(`*Tienes 3 segundos para eliminar el link y retractarte...!!!!*`)
-    conn.fakeReply(m.chat, sticker1, '0@s.whatsapp.net', '🔥 THE SHADOW BROKERS - BOT 🔥', 'status@broadcast', null, m, { type: 'stickerMessage', sticker: true })
-    await conn.fakeReply(m.chat, sticker2, 'ShadowBotV3')
-    await conn.fakeReply(m.chat, sticker3, 'ShadowBotV3')
+    await m.reply(`*3!!*`)
+    await m.reply(`*2!!*`)
+    await m.reply(`*1!!*`)
     if (isAdmin) return m.reply('*Te salvaste cagon(a) eres admin, no puedo eliminarte :v*')
     if (!isBotAdmin) return m.reply('*El bot no es admin, no puede exterminar a las personas*')
     let linkGC = ('https://chat.whatsapp.com/' + await this.groupInviteCode(m.chat))
