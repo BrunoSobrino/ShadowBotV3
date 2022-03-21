bf886ea0f666b33b15e6a9707593b96a2c41a2af
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-21 11:25:13 -0600

    ©ShadowBot

--- plugins/yts.js
+++ plugins/yts.js
@@ -5,22 +5,21 @@ let handler = async (m, { text }) => {
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-*${v.title}* (${v.url})
-Duración: ${v.timestamp}
-Fecha de subida: ${v.ago}
-${v.views} vistas
+✨ *${v.title}* 
+🎈 (${v.url})
+⏳ *Duración:* ${v.timestamp}
+🗓 *Fecha:* de subida: ${v.ago}
+👀 *Vistas:* ${v.views} 
       `.trim()
       case 'channel': return `
-*${v.name}* (${v.url})
-_${v.subCountLabel} (${v.subCount}) Subscriber_
-${v.videoCount} video
+✨ *${v.name}*
+🎈 (${v.url})
+👥 *Subscriptores:* ${v.subCountLabel}  (${v.subCount}) 
+🎥 *Videos:* ${v.videoCount}  
 `.trim()
     }
   }).filter(v => v).join('\n========================\n')
   m.reply(teks)
 }
-handler.help = ['', 'earch'].map(v => 'yts' + v + ' <pencarian>')
-handler.tags = ['General']
 handler.command = /^yts(earch)?$/i
-
 module.exports = handler
