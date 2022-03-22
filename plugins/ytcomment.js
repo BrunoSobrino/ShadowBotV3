88b690a57a4c3fe886ab0bc2864244f989d862d6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-03-21 18:43:40 -0600

    ©ShadowBot

--- plugins/ytcomment.js
+++ plugins/ytcomment.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, text }) => {
-if (!text) throw '*[❗️] Estas usando mal el comando!!*\n*Ejemplo:* *#ytcomment Suscríbete al canal oficial'
+if (!text) throw '*[❗️] Estas usando mal el comando!!*\n*Ejemplo de uso:*\n*#ytcomment Suscríbete al canal oficial*'
 conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
 avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
 comment: text,
