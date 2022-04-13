0b95e1eb630664d954a22cf69f44c213009fd17e
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:59:00 -0500

    ©ShadowBot

--- plugins/randomnsfw.js
+++ plugins/randomnsfw.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 let handler = async(m, { conn }) => {
-if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'let les = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/nsfw')
 conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}`, m)
 }
 handler.help = ['randomnsfw']
