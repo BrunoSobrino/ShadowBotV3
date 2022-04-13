3e2243ffba07553a31a8efea6c58a06e0e7c7257
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:11:36 -0500

    ©ShadowBot

--- plugins/nekogif.js
+++ plugins/nekogif.js
@@ -7,24 +7,17 @@ const gs = require('nekos.life')
 const neko = new gs()
 
 let handler  = async (m, { conn, text }) => {
-  pp = (await neko.sfw.nekoGif()).url
-                     await sticker(false, pp, 'SFW Neko', author).then(gege => {
-                     conn.sendMessage(m.chat, gege, 'stickerMessage', { quoted: m })
-                     })
-  //if (!text) throw 'Uhm...Teksnya?'
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
+pp = (await neko.sfw.nekoGif()).url
+await sticker(false, pp, 'SFW Neko', author).then(gege => {
+conn.sendMessage(m.chat, gege, 'stickerMessage', { quoted: m })
+})
+//if (!text) throw 'Uhm...Teksnya?'
 }
 handler.help = ['nekogif (sfw)']
 handler.tags = ['wibu']
 handler.command = /^nekogif$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
 handler.group = false
 handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
 handler.fail = null
-
 module.exports = handler
