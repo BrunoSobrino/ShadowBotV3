0beefaab3c4849f52481b0110130d43f5fb116ff
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:26:56 -0500

    ©ShadowBot

--- plugins/sideboobs.js
+++ plugins/sideboobs.js
@@ -2,23 +2,13 @@ let axios = require('axios');
 let fetch = require('node-fetch')
 let neko = require('nekos.life')
 let Neko = new neko()
-     let handler  = async (m, { conn, args }) => {
-     json = (await axios.get('https://meme-api.herokuapp.com/gimme/sideboobs')).data
-   conn.sendFile(m.chat, json.url, 'boobs.jpg', json.title, m, false)
+let handler  = async (m, { conn, args }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
+json = (await axios.get('https://meme-api.herokuapp.com/gimme/sideboobs')).data
+conn.sendFile(m.chat, json.url, 'boobs.jpg', json.title, m, false)
 }
 handler.help = ['sideboobs']
 handler.tags = ['sange']
 handler.command = /^sideboobs$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.limit = false
-handler.admin = false
-handler.botAdmin = false
-
 handler.fail = null
-
 module.exports = handler
