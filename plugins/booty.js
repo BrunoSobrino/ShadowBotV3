39a287d71e720fd00d7d5f0910a681b9b555dec6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 22:58:05 -0500

    ©ShadowBot

--- plugins/booty.js
+++ plugins/booty.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 let handler = async(m, { conn, usedPrefix, command }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'    
 let res = await axios("https://meme-api.herokuapp.com/gimme/booty")
 let json = res.data
 let ShadowBot = json.url
