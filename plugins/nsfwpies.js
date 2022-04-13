542abb6ae7524645b41cc3e333d76c83363675dd
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:19:00 -0500

    ©ShadowBot

--- plugins/nsfwpies.js
+++ plugins/nsfwpies.js
@@ -1,5 +1,6 @@
 let fetch = require('node-fetch')
 let handler  = async (m, { conn, usedPrefix, command }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 heum = await fetch(`https://api-alc.herokuapp.com/api/nsfw/foot?apikey=ConfuMods`)
 json = await heum.buffer()
 conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)}
