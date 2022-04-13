ea2f4ccb569afdcec7c69ed5467f8a700a294446
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:23:59 -0500

    ©ShadowBot

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -1,23 +1,12 @@
 const axios = require('axios')
 let handler = async(m, { conn }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 let porn = await axios.get('https://meme-api.herokuapp.com/gimme/pussy')
-           conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
-  }
+conn.sendFile(m.chat, `${porn.data.url}`, '', `${porn.data.title}`, m)
+}
 handler.help = ['pussy']
 handler.tags = ['images']
 handler.command = /^(pussy)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-handler.register = false
-
-handler.admin = false
-handler.botAdmin = false
-
 handler.fail = null
 handler.exp = 0
-handler.limit = false
-
 module.exports = handler
