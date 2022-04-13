9db0ee256947a9cf32078c3b521650928eab0140
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-12 23:21:46 -0500

    ©ShadowBot

--- plugins/panties.js
+++ plugins/panties.js
@@ -1,5 +1,6 @@
 let axios = require("axios")
 let handler = async (m, { conn, usedPrefix, command }) => {
+if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 let res = await axios("https://meme-api.herokuapp.com/gimme/panties")
 let json = res.data
 let url = json.url
