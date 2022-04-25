6b27c902653aadad6c1115d0b9c82c387038fcf6
BrunoSobrino <90165013+BrunoSobrino@users.noreply.github.com>
2022-04-24 21:10:56 -0500

    ©ShadowBot

---
+++ plugins/play.1.2.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+let handler = async (m, {command, conn, text}) => {
+if (command == 'play.1') {
+let espera = '*[❗𝐈𝐍𝐅𝐎❗] 𝙰𝙶𝚄𝙰𝚁𝙳𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝙰𝚄𝙳𝙸𝙾*'
+m.reply(espera)
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.url, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
+if (command == 'play.2') {
+let espera = '*[❗𝐈𝐍𝐅𝐎❗] 𝙰𝙶𝚄𝙰𝚁𝙳𝙴 𝚄𝙽 𝙼𝙾𝙼𝙴𝙽𝚃𝙾 𝙴𝙽 𝙻𝙾 𝚀𝚄𝙴 𝙴𝙽𝚅𝙸𝙾 𝚂𝚄 𝚅𝙸𝙳𝙴𝙾*'
+m.reply(espera)
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp4?text="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.url, 'error.mp4', `_The Shadow Brokers - Bot_`, m)}
+}
+handler.command = ['play.1', 'play.2']
+module.exports = handler
